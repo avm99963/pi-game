3b38a7d35ebb1f3c151fafec94f5d67e2b102af4
Adrià <jocdeladria@gmail.com>
2013-11-15 22:22:31 +0100

    Bug fixes and optimization
    *Now the Pi Number written in the screen will wrap and do a break so it
    can be shown correctly
    *The code is now more optimized

--- scripts.js
+++ scripts.js
@@ -1,25 +1,24 @@
+var pi, i = -2, lost = false, first = true;
+
 window.onload = function() {
 	var client = new XMLHttpRequest();
 	client.open('GET', 'pi.txt');
 	client.onreadystatechange = function() {
-	  window.pi = client.responseText;
+	  pi = client.responseText;
 	}
 	client.send();
 	var pispan = document.getElementById("pinumber");
-	window.lost = false;
-	window.first = true;
-	window.i = -2;
 	document.body.onkeydown = function(event)
 	{
-		if (window.lost === false)
+		if (lost === false)
 		{
 			var number = event.keyCode;
 			if ((number > 47 && number < 58) || number == 190)
 			{
 				if (i > 78)
-				{
 					pispan.style.fontSize = "20px";
-				}
+                if (first)
+                    document.getElementById("introductiontext").parentNode.removeChild(document.getElementById("introductiontext"));
 				var codes = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 190];
 				var characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 				var pidigit = pi.charAt(0);
@@ -28,30 +27,26 @@ window.onload = function() {
 				console.log("Entry: "+entry+" // Pidigit: "+pidigit);
 				if (pidigit == entry)
 				{
-					window.i++;
-					if (window.first === true)
+					i++;
+					if (first === true)
 					{
-						window.first = false;
-						pispan.innerHTML = "<span class='good'>"+entry+"</span>";
+						first = false;
+						pispan.innerHTML = entry;
 					}
 					else
-					{
-						pispan.innerHTML = pispan.innerHTML+"<span class='good'>"+entry+"</span>";
-					}
+						pispan.innerHTML = pispan.innerHTML+entry;
 					pi = pi.substr(1);
 				}
 				else
 				{
-					if (window.first === true)
+					if (first === true)
 					{
-						window.first = false;
+						first = false;
 						pispan.innerHTML = "<span class='wrong'>"+entry+"</span>";
 					}
 					else
-					{
 						pispan.innerHTML = pispan.innerHTML+"<span class='wrong'>"+entry+"</span>";
-					}
-					window.lost = true;
+					lost = true;
 				}
 			}
 		}
